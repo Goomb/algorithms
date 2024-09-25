cd4a71281051f8fff58eb402eb03531d55d53db3
Stepan Kashuba <ihaterabbids@gmail.com>
2024-09-25 16:30:29 +0500

    Add revert polish calc

---
+++ revert-polish.php
@@ -0,0 +1,35 @@
+<?php
+
+function revertPolish(array $data): int
+{
+    $operators = ['+', '-', '*'];
+    $stack = [];
+    foreach($data as $operand) {
+        if (!in_array($operand, $operators)) {
+            $stack[] = $operand;
+        } else {
+            if ($operand === '+') {
+                $stack[] = array_pop($stack) + array_pop($stack);
+            }
+
+            if ($operand === '*') {
+                $stack[] = array_pop($stack) * array_pop($stack);
+            }
+
+            if ($operand === '-') {
+                $stack[] = -1 * array_pop($stack) + array_pop($stack);
+            }
+        }
+    }
+
+    return array_pop($stack);
+}
+
+$result = revertPolish([1, 2, '+']);
+var_dump($result === 3);
+
+$result = revertPolish([1, 2, 3, '*', '+']);
+var_dump($result === 7);
+
+$result = revertPolish([1, 2, 3, '*', '+', 2, '-']);
+var_dump($result === 5);
